4ebb624001f496f75aca1c500dce7a52677be09c
yasucaria <ya_sucaria@hotmail.com>
2020-05-28 17:58:03 -0300

    app use

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var registro = require('./routes/registro');
+var producto = require('./routes/producto');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/registro', registro);
+app.use('/producto', producto);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
