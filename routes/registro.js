47493ddf2a9d9099c0d75b08b25d2fff59135de6
yasucaria <ya_sucaria@hotmail.com>
2020-05-29 18:10:26 -0300

    json productos

--- routes/registro.js
+++ routes/registro.js
@@ -4,5 +4,6 @@ const registroController = require("../controllers/registroController")
 
 router.get('/registro', registroController.registro);
 
+router.post('/registro', registroController.registro);
 
 module.exports = router;
