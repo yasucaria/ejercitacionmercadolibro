7af9c9a57c50abd86d93c27f17010321cf58509e
francoponce-cloud <franponce98@gmail.com>
2020-05-31 20:24:15 -0300

    Se elimina ruta carrito, modifica modularización de ruta producto

--- routes/producto.js
+++ routes/producto.js
@@ -5,10 +5,10 @@ const productosController = require("../controllers/productosController.js");
 router.get("/", productosController.listado);
 router.post("/", productosController.alta);
 router.get("/:idProducto", productosController.detalle);
-router.get("/create", productosController.alta);
+router.get("/create", productosController.agregar);
 router.get("/:idProducto/edit", productosController.modificacion);
 router.put("/:idProducto", productosController.modificacion);
 router.delete("/:idProducto", productosController.baja);
-router.get("/agregar", productosController.agregar);
+router.get("/agregar", productosController.alta);
 
 module.exports = router;
