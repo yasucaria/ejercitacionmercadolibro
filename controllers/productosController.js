d3e54784458d004a6438640ca47f0031491f8844
yasucaria <ya_sucaria@hotmail.com>
2020-05-31 20:05:32 -0300

    se resuelve renderizacion de /create

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -3,6 +3,7 @@ const listadoProductos = JSON.parse(fs.readFileSync('./data/detalleProductos.jso
 
 const productosController = {
 "listado" : function(req, res){
+    
     res.render("listadoProductos", {listadoProductos: listadoProductos});
 },
 "detalle" : function(req, res){
@@ -10,11 +11,8 @@ let idProductoElegido = req.params.idProducto;
 const productoClickeado = listadoProductos.find(producto => req.params.idProducto)
     res.render("detalleProducto", {title: req.params.idProducto});
 },
-"agregar" : function(req, res){
+"alta" : function(req, res){
     res.render("agregarProducto")
-},
-"create": function(req, res){
-    res.render("")
 },
 "modificacion": function(req, res){
     res.render("")
